ff838e8b4ae1dcbc644cced4d44ae7511ce1b626
Jason Alvarado <jayxalvarado@gmail.com>
2023-09-14 05:48:24 -0700

    init

--- counter.js
+++ counter.js
@@ -2,7 +2,8 @@ export function setupCounter(element) {
   let counter = 0;
   const setCounter = (count) => {
     counter = count;
-    element.innerHTML = `How many &#127814 you have taken ${counter}`;
+    element.innerHTML = `How many &#127814 you have taken = 
+    ${counter}`;
   };
   element.addEventListener("click", () => setCounter(counter + 1));
   setCounter(0);
