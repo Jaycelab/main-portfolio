ffc57bc4b9ef3167f0006127e24dff98750bbbd4
Jason Alvarado <jayxalvarado@gmail.com>
2023-09-14 06:03:24 -0700

    init

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ document.querySelector("#app").innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="./src/imgs/0ikB3cXoAESHrc.jpg" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Brian And Boyet!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click Above For Magic
     </p>
   </div>
 `;
